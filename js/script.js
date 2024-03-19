42fdd6c0476b5237ca5c43c786d6b4e475533871
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-03-19 23:11:44 +0100

    add-comments

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*Descrizione:
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
+
+// ? Genera 5 numeri casuali
+// ? Mostra i numeri in pagina
+// ? Rimuovi i numeri dalla pagina
+// ? Chiedi all'utente di inserire i numeri
+// ? Verifica quali numeri sono stati indovinati
+// ? Mostra i risultati
+
