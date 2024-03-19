82b1f551a7eb04d6bf35d0792f58ca0c8c72eb80
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-03-19 23:23:49 +0100

    div-add

--- js/script.js
+++ js/script.js
@@ -4,9 +4,20 @@ Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i 
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
 
 // ? Genera 5 numeri casuali
+    const randomNumbers = [];
+    //Generare un numero casuale tra 0 e 50
+    for (let i = 0; i < 5; i++) {
+        randomNumbers.push(Math.floor(Math.random() * 100));
+    }
 // ? Mostra i numeri in pagina
+    //*Creo un div
+    const numbersDiv = document.createElement('div');
+    
 // ? Rimuovi i numeri dalla pagina
+
 // ? Chiedi all'utente di inserire i numeri
+
 // ? Verifica quali numeri sono stati indovinati
+
 // ? Mostra i risultati
 
